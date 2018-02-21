daadd20f534392bed31b9303bb8d190c01a6bff5
Akisole <32121754+Akisole@users.noreply.github.com>
2018-02-21 09:51:15 +0300

    Create Form1.cs

---
+++ Filtres/Form1.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Filtres
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap image;
+        public Form1()
+
+        {
+            InitializeComponent();
+        }
+
+        private void aToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName);
+
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+
+        }
+        
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpeg; *.bmp | All Files (*.*) | *.*";
+            dialog.Title = "Save an Image File";
+            dialog.ShowDialog();
+
+            if (pictureBox1.Image != null)
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                    pictureBox1.Image.Save(dialog.FileName);
+            }
+      
+        }
+
+        private void инверсияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InvertFilter filter = new InvertFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+      //      Bitmap resultImage = filter.processImage(image);
+       //     pictureBox1.Image = resultImage;
+         //   pictureBox1.Refresh();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap newImage = ((Filters)e.Argument).processImage(image, backgroundWorker1);
+            if (backgroundWorker1.CancellationPending != true)
+                image = newImage;
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+            progressBar1.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void размытиеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BlurFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void гауссаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new GaussianFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void чбToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new GreyScaleFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void сепияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new SepiaFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void яркостьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BrightFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void резкостьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new SharpnessFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void поОсиYToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new SobelYFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void поОсиXToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new SobelXFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void поОсиYToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BorderYShFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void поОсиXToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BorderXShFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void поОсиYToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BorderYPFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void поОсиXToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BorderXPFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+
+        }
+
+        private void переносToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new PerenosFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void серыйМирToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new GreyWorldFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void расширениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new DelatiomFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+
+
+
+
+
+
+
+    }
+}
